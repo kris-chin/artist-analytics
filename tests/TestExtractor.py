199e10c4e1309fbfab4b41cf1c9ee96fc352d2f8
Krischin Layon <krischinlayon@gmail.com>
2021-12-29 20:02:37 -0800

    Added Skeleton Code for Extractor Tests

---
+++ tests/TestExtractor.py
@@ -0,0 +1,15 @@
+import pytest
+
+#Tests if Dataframe is properly created from a CSV/TSV/Excel Extactor
+
+#Tests if Dataframe is properly created from a JSON/API Extractor
+
+#Tests if Dataframe is Easily Joinable (easy to integrate into pipeline)
+    #This is similar to the next suite below:
+
+#Tests if Dataframe is Properly Cleaned:
+    #Test if there are duplicates
+    #Test if there are empty values
+    #Test if correctly formatted (eg. time dates)
+
+#Tests if Extractor provides Source Dates
