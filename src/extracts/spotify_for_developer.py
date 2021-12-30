a5ad07a1f7c12d47697e139cc81518abb58dd1fc
Krischin Layon <krischinlayon@gmail.com>
2021-12-29 21:07:07 -0800

    Created Generalized API Call method for SpotifyForDevelopers

--- src/extracts/spotify_for_developer.py
+++ src/extracts/spotify_for_developer.py
@@ -11,11 +11,20 @@ class SpotifyForDevelopers:
         else:
             self.authToken = authToken
     
-    def GetSongData(self, song_id: str):
+    #Private Methods for making API calls. We don't want to be using these directly, instead, we'll call these in bulk for a "Complete" Extaction
+    def __MakeAPICall(self, endpoint, query=None):
         try:
             l.debug("Attempting to make API call to spotify")
-            re = r.get("https://api.spotify.com/v1/tracks/" + song_id, headers={'Authorization' : "Bearer " + self.authToken})
+            
+            #Set up Header
+            headers={'Authorization' : "Bearer " + self.authToken}
+
+            #Make the appropriate GET request
+            if query == None: re = r.get(endpoint, headers=headers)
+            else: re = r.get(endpoint, params=query, headers=headers)
+
             if re.status_code == 404: l.error("ERROR 404")
+            elif re.status_code == 400: l.error("ERROR 400:" + re.text)
             else: return eval(re.text.replace('true',"True").replace('false',"False")) #Return response as a JSON object
         except Exception as e:
             l.error("API Call recieved an error:")
