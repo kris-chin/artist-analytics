c2c31c6ef902127c98900396006bf9bd4d24eac0
Krischin Layon <krischinlayon@gmail.com>
2021-12-29 22:54:24 -0800

    First Distrokid Test File

---
+++ tests/test_distrokid.py
@@ -0,0 +1,19 @@
+import pytest
+import pandas
+from pathlib import Path
+
+#Get Home Directory
+p = str(Path('.').resolve().parents[0]) + "/data/DistroKid.tsv"
+
+class TestDistrokid:
+
+    #Is Pipeline Importable?
+    def test_import(self):
+        try:
+            from src.pipelines.distrokid import Distrokid
+            distrokid = Distrokid(p)
+            assert 1
+
+        except:
+            assert 0
+    
