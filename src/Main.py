3df113504a087481dfaf821a177fc10f17922d18
Krischin Layon <krischinlayon@gmail.com>
2021-12-29 19:14:14 -0800

    Create Prototype SpotifyForDevelopers Pipeline

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 import logging as l
 
+#Import Authorization Functions
+import Auth
+
 #Import Extractors
 import extracts.distrokid
 import extracts.spotify_for_artists
+import extracts.spotify_for_developer
 
 if __name__ == "__main__":
 
@@ -23,10 +27,15 @@ if __name__ == "__main__":
 
     #Create extractors
     distrokid = extracts.distrokid.Distrokid(filenames['distrokid'])
-    spotify_for_artists = extracts.spotify_for_artists.SpotifyForArtists((filenames['recordings'], "N/A"), (filenames['playlists'], "N/A"), (filenames['audience'], "N/A"))
+    spotify_for_artists = extracts.spotify_for_artists.SpotifyForArtists(
+        (filenames['recordings'], "N/A"), (filenames['playlists'], "N/A"), (filenames['audience'], "N/A")
+    )
+    spotify_for_developer = extracts.spotify_for_developer.SpotifyForDevelopers(Auth.GenerateSpotifyAuthToken())
 
     #Test
     print(distrokid.GetDataframe())
     print(spotify_for_artists.GetRecordingsDataframe())
     print(spotify_for_artists.GetPlaylistsDataframe())
     print(spotify_for_artists.GetAudienceDataframe())
+
+    print(spotify_for_developer.GetSongData("1gjhtLqLPbYwIAWWkwy4Uj"))
