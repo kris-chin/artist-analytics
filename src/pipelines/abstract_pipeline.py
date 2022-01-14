b145a1b42f4562e111b53893edbda940f58dd6ed
Krischin Layon <krischinlayon@gmail.com>
2022-01-13 20:26:56 -0800

    update abstract_pipeline

--- src/pipelines/abstract_pipeline.py
+++ src/pipelines/abstract_pipeline.py
@@ -8,7 +8,12 @@ class Pipeline(ABC):
     @abstractmethod
     def __init__(self):
         self.__dataframe : pandas.DataFrame = NotImplemented
+        self.__name : str = NotImplemented
 
     @abstractmethod
     def GetDataframe(self):
         return self.__dataframe
+
+    @property
+    def name(self):
+        return self.__name
