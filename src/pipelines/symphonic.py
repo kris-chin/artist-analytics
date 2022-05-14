571b8d2e131d259c8d6e4516f8c7e3c96c442c13
Krischin Layon <krischinlayon@gmail.com>
2022-05-14 18:23:09 +0000

    Added sale_month formatting for symphonic

--- src/pipelines/symphonic.py
+++ src/pipelines/symphonic.py
@@ -20,6 +20,41 @@ class Symphonic(Pipeline):
 
     def GetDataframe(self):
 
+        #We have to clean the "Reporting Period" data. 
+        #Input: YY-MMM (last two digits of year : first three letters of month with caps on first letter)
+        #Output: YYYY-MM (all digits)
+        #Set up map for month conversion
+        #(Maybe this could be placed somewhere else?)
+        month_map = {
+            'Jan' : '01',
+            'Feb' : '02',
+            'Mar' : '03',
+            'Apr' : '04',
+            'May' : '05',
+            'Jun' : '06',
+            'Jul' : '07',
+            'Aug' : '08',
+            'Sep' : '09',
+            'Oct' : '10',
+            'Nov' : '11',
+            'Dec' : '12'
+        }
+
+        #transformation function for 'Reporting Period
+        def transform_data(date: str) -> str:
+            #split date with -
+            #this gives a list of ["last two digits of year" , "First three letters of month, first letter capitalized"]
+            split = date.split('-')
+            #Assemble a new string based on our map
+            try:
+                return ("20" + str(split[0]) + "-" + month_map.get(split[1]) )
+            except:
+                l.error("'Reporting Period' Data does not match expected formatting. Check column")
+                return None
+
+        #Apply transformation
+        self.__dataframe['Reporting Period'] = self.__dataframe['Reporting Period'].apply(transform_data)
+
         columns={
             'Track Artists' : 'artist',
             'Track Title' : 'song_title',
@@ -28,10 +63,9 @@ class Symphonic(Pipeline):
             'Count' : 'quantity',
             'Digital Service Provider' : 'store',
             'UPC Code' : 'upc',
-            'Territory' : 'country'
+            'Territory' : 'country',
+            'Reporting Period' : 'sale_month'
         }
-
-        #TODO: reformat "reporting period" to match "sale month" formatting
 
         self.__dataframe = self.__dataframe.rename(columns=columns)
 
